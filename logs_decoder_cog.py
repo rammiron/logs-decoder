ef54831c055221c8217d99e990c378e6ad23b073
rammiron <rammiron666@gmail.com>
2024-07-28 20:07:00 +0300

    added ref to virustotal

--- logs_decoder_cog.py
+++ logs_decoder_cog.py
@@ -54,6 +54,9 @@ class LogsCheckerCog(commands.Cog):
                           "Сам файл разработан нашими программистами,"
                           " не содержит вирусов, "
                           "но для большей уверенности проверен на вирусы с помощью сервиса virustotal.\n"
+                          "-> [ТЫК](https://www.virustotal.com/gui/file"
+                          "/d34729fdec9fcb647ea54cdc43b362aee983c60dcce104917c61bf9f6c0ca4cf)"
+                          "\n"
                           "Просим отправить в этот чат сгенерированный файл.")
         await ctx.channel.send("Файл: ", file=file)
 
